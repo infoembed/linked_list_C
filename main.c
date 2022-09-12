a7243c2740d899a3cf0e1c909a1103d88554ba9d
Ibrahim BOUDJENAH <ibra_boudjenah@outlook.fr>
2022-09-12 13:04:59 +0100

    linkedlist using structure

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+typedef struct node {
+int data;
+struct node * next;
+}node_t;
+
+void print_list(node_t* head)
+{
+    int i = 0;
+    while(head){
+
+        printf("node %d ; value %d \n",i++, head->data);
+        head = head->next;
+
+    }
+}
+
+int main(){
+   
+    node_t third = {3, NULL};
+    node_t second = {2, &third};
+    node_t first = {1, &second};
+
+    node_t* head = &first;
+    
+
+    print_list(head);
+
+
+
+   return 0;
+
+}
